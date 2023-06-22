c6733a1ab066e406f39a3497617ba1397087b71b
Paige Thompson <paige@paige.bio>
2023-06-21 19:29:04 -0700

    fix script

--- create_pdf.sh
+++ create_pdf.sh
@@ -5,5 +5,3 @@ hugo serve &> /dev/null &
 python3 -c 'from selenium import webdriver; options = webdriver.ChromeOptions(); options.add_argument("--headless=new"); driver = webdriver.Chrome(options=options); driver.get("http://localhost:1313"); print(driver.print_page());' | base64 -d > static/dl/cv.pdf
 
 killall hugo
-
-rm -r public/
